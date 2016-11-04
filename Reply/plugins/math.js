8a35f098cd7251178e3c82d47b064dbd34ee1039
Mark Lyck <mark.lyck@gmail.com>
2016-11-03 19:49:55 -0500

    Can answer math questions

--- Reply/plugins/math.js
+++ Reply/plugins/math.js
@@ -1,22 +1,58 @@
 var _ = require('lodash')
+var mathjs = require('mathjs')
+
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this
+    return target.split(search).join(replacement)
+}
+
+const operators = [
+  '+', '-', '/', '*',
+  'plus', 'add', 'minus', 'subtract', 'divide', 'divded', 'multiply', 'multiplied', 'times'
+]
+
+const normalizeOperators = (msg) => {
+
+  const plus = ['plus', 'add']
+  const minus = ['minus', 'subtract']
+  const multiply = ['multiply', 'multiplied', 'times']
+  const divide = ['divide', 'divided']
+
+  plus.forEach(operator => { msg = msg.replaceAll(operator, '+') })
+  minus.forEach(operator => { msg = msg.replaceAll(operator, '-') })
+  multiply.forEach(operator => { msg = msg.replaceAll(operator, '*') })
+  divide.forEach(operator => { msg = msg.replaceAll(operator, '/') })
+
+  return msg
+}
+
+const removeJunkChars = (message, msgObj) => {
+
+  let junklessMessage = message.split(' ')
+  junklessMessage = junklessMessage.filter(word => {
+    if (operators.indexOf(word) > -1 || msgObj.numbers.indexOf(word) > -1) {
+      return true
+    } else {
+      return false
+    }
+  })
+  return junklessMessage
+}
 
 const math = (msgObj) => {
   return new Promise(resolve => {
-    console.log('MATH');
-    if (msgObj.reply)
+    if (msgObj.reply) {
       resolve(msgObj)
-
-    const operators = [
-      '+', '-', '/', '*',
-      'plus', 'minus', 'divide', 'divded', 'multiply', 'multiplied'
-    ]
-    console.log('OPERATORS: ', _.intersection(msgObj.cleanedMessage.split(' '), operators).length)
-    console.log('NUMBERS: ', msgObj.numbers);
-    if (msgObj.numbers.length >= 2 && _.intersection(msgObj.cleanedMessage.split(' '), operators).length > 0) {
-      console.log('MATH OPERATION!!!');
-      msgObj.reply = 'Math operation'
+    } else {
+      // TODO improve this, by making sure the operator is inbetween the numbers.
+      if (msgObj.numbers.length >= 2 && _.intersection(msgObj.cleanedMessage.split(' '), operators).length > 0) {
+        let mathOperation = normalizeOperators(msgObj.cleanedMessage)
+        mathOperation = removeJunkChars(mathOperation, msgObj)
+        const result = mathjs.eval(mathOperation.join(' '))
+        msgObj.reply = result
+      }
+      resolve(msgObj)
     }
-    resolve(msgObj)
   })
 }
 
